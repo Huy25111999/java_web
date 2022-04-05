89b7495b4d0e348d817453a4e9420d9d4db866e1
Huy Support <82765149+Huy25111999@users.noreply.github.com>
2022-04-05 17:46:38 +0700

    Bug

--- mapNhatKy.js
+++ mapNhatKy.js
@@ -1,4 +1,5 @@
 
+
 document.getElementById("menu-toggle").onclick=function (){
     if (this.classList.contains('move-in'))
     {
@@ -13,7 +14,38 @@ document.getElementById("menu-toggle").onclick=function (){
     }
 }
 
-// Time
+$(document).ready(function() {
+    marker();
+});
+// marker
+function marker(){
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaHV5c3VwcG9ydCIsImEiOiJjbDB2dDV1d3kxYWF6M2lvMmo0Mml3MWJ2In0.sBwz__9G5aSTCROFnlKrYw';
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-74.5, 40],
+        zoom: 8,
+    });
+    const marker1 = new mapboxgl.Marker()
+        .setLngLat([-74.5, 40])
+        .addTo(map);
+}
+
+/*document.getElementById("menu-toggle").onclick=function (){
+    if (this.classList.contains('move-in'))
+    {
+        document.getElementById("sidebar-content").style.display="block";
+        this.classList.add('move-out');
+        this.classList.remove('move-in');
+    }else
+    {
+        document.getElementById("sidebar-content").style.display="none";
+        this.classList.add('move-in');
+        this.classList.remove('move-out');
+    }
+}
+
+
 var dateClickStart = document.getElementById("date_timepicker_start1");
 var dateClickEnd = document.getElementById("date_timepicker_start2");
 
@@ -35,17 +67,22 @@ dateClickEnd.onmouseleave=function ()
     document.getElementById("datepickersEnd").style.display='none';
 }
 
-// main
+
 $(document).ready(function() {
-     select();
+    select();
     marker();
     timepicker();
 
 });
 function timepicker()
 {
-    $("#date_timepicker_start1").datepicker();
-    $("#date_timepicker_start2").datepicker();
+    $("#date_timepicker_start1").datepicker({
+        format:"DD/MM/YY HH:mm",
+
+    });
+    $("#date_timepicker_start2").datepicker({
+        format:"DD/MM/YY HH:mm",
+    });
     $(".datepicker>div").attr('id','datepickersStart');
     document.getElementById("datepickersStart").style.display='none';
     $("#date_timepicker_start2>.datepicker>div").attr('id','datepickersEnd');
@@ -61,11 +98,11 @@ function select(){
         dataType:"json",
         success:function(res)
         {
-             var data= res.content.data.data;
-             for ( var i=0; i < data.length;i++)
-             {
-                 select += '<option value="'+data[i].id+'" data-select2-id="4">'+data[i].imei+'</option>';
-             }
+            var data= res.content.data.data;
+            for ( var i=0; i < data.length;i++)
+            {
+                select += '<option value="'+data[i].id+'" data-select2-id="4">'+data[i].imei+'</option>';
+            }
             select += '</select>';
             document.getElementById('selectDevice').innerHTML = select;
             console.log(res.content.data.data);
@@ -78,7 +115,7 @@ function select(){
     })
 
 }
-// marker
+
 function marker(){
     mapboxgl.accessToken = 'pk.eyJ1IjoiaHV5c3VwcG9ydCIsImEiOiJjbDB2dDV1d3kxYWF6M2lvMmo0Mml3MWJ2In0.sBwz__9G5aSTCROFnlKrYw';
     const map = new mapboxgl.Map({
@@ -92,48 +129,9 @@ function marker(){
         .addTo(map);
 }
 
-// Table
-document.getElementById("btn-seach").onclick=function ()
-{
-    var timeStart =$("#date_timepicker_start").val();
-    var timeEnd = $("#date_timepicker_end").val();
-    var roles = $("#imeiDevice").val();
-    console.log(timeStart);
-    console.log(timeEnd);
-    console.log(roles);
 
-    var item = new FormData();
-    item.id=roles;
-    item.start_time = timeStart;
-    item.end_time = timeEnd;
-    var table = '<table class="table table-striped table-hover table-condensed table-export" id="table-history-view">\n' +
-        '    <thead id="theadExport">\n' +
-        '    <tr>\n' +
-        '        <th className="text-center">STT</th>\n' +
-        '        <th>Trạng thái</th>\n' +
-        '        <th >Thời gian</th>\n' +
-        '    </tr>\n' +
-        '    </thead>\n' +
-        '    <tbody>';
-    $.ajax({
-        url:"/loadLocationHistory",
-        method:"POST",
-        dataType: 'json',
-        data:JSON.stringify(item),
-        success:function (res){
-            alert(1);
-            table +=' <tr></tr>';
-            table +='</tbody>';
-            table += '</table>';
-            document.getElementById('table').innerHTML = table;
-        },
-        error:function (err){
 
-        }
-    })
-}
 
-// Table
 document.getElementById("btn-seach").onclick=function ()
 {
     var timeStart =$("#date_timepicker_start").val();
@@ -175,3 +173,4 @@ document.getElementById("btn-seach").onclick=function ()
     })
 }
 
+ */
