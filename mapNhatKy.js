ecd072b2e1aadf2f501e7801be851195e596b7a0
Huy Support <82765149+Huy25111999@users.noreply.github.com>
2022-04-06 17:36:14 +0700

    fix

--- mapNhatKy.js
+++ mapNhatKy.js
@@ -1,5 +1,4 @@
 
-
 document.getElementById("menu-toggle").onclick=function (){
     if (this.classList.contains('move-in'))
     {
@@ -14,80 +13,28 @@ document.getElementById("menu-toggle").onclick=function (){
     }
 }
 
-$(document).ready(function() {
-    marker();
-});
-// marker
-function marker(){
-    mapboxgl.accessToken = 'pk.eyJ1IjoiaHV5c3VwcG9ydCIsImEiOiJjbDB2dDV1d3kxYWF6M2lvMmo0Mml3MWJ2In0.sBwz__9G5aSTCROFnlKrYw';
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-74.5, 40],
-        zoom: 8,
-    });
-    const marker1 = new mapboxgl.Marker()
-        .setLngLat([-74.5, 40])
-        .addTo(map);
-}
-
-/*document.getElementById("menu-toggle").onclick=function (){
-    if (this.classList.contains('move-in'))
-    {
-        document.getElementById("sidebar-content").style.display="block";
-        this.classList.add('move-out');
-        this.classList.remove('move-in');
-    }else
-    {
-        document.getElementById("sidebar-content").style.display="none";
-        this.classList.add('move-in');
-        this.classList.remove('move-out');
-    }
-}
-
-
-var dateClickStart = document.getElementById("date_timepicker_start1");
-var dateClickEnd = document.getElementById("date_timepicker_start2");
-
-dateClickStart.onclick=function ()
-{
-    document.getElementById("datepickersStart").style.display='block';
-}
-dateClickStart.onmouseleave=function ()
-{
-    document.getElementById("datepickersStart").style.display='none';
-}
-
-dateClickEnd.onclick=function ()
-{
-    document.getElementById("datepickersEnd").style.display='block';
-}
-dateClickEnd.onmouseleave=function ()
-{
-    document.getElementById("datepickersEnd").style.display='none';
-}
-
-
+// main
 $(document).ready(function() {
     select();
     marker();
     timepicker();
 
-});
+})
+
+//Time
 function timepicker()
 {
-    $("#date_timepicker_start1").datepicker({
-        format:"DD/MM/YY HH:mm",
+    flatpickr('#date_timepicker_end',{
+        enableTime: true,
+        dateFormat: 'd-m-Y,h:m',
+    });
+    flatpickr('#date_timepicker_start',{
+        enableTime: true,
+        dateFormat: 'd-m-Y,h:m',
+    });
 
-    });
-    $("#date_timepicker_start2").datepicker({
-        format:"DD/MM/YY HH:mm",
-    });
-    $(".datepicker>div").attr('id','datepickersStart');
-    document.getElementById("datepickersStart").style.display='none';
-    $("#date_timepicker_start2>.datepicker>div").attr('id','datepickersEnd');
-    document.getElementById("datepickersEnd").style.display='none';
 }
+
 
 function select(){
     var select = "<select id=\"imeiDevice\" data-placeholder=\"Thiết bị\" class=\"single-select select2-hidden-accessible\" style=\"width: 100%\" data-select2-id=\"imeiDevice\" tabindex=\"-1\" aria-hidden=\"true\">";
@@ -115,14 +62,14 @@ function select(){
     })
 
 }
-
+// marker
 function marker(){
     mapboxgl.accessToken = 'pk.eyJ1IjoiaHV5c3VwcG9ydCIsImEiOiJjbDB2dDV1d3kxYWF6M2lvMmo0Mml3MWJ2In0.sBwz__9G5aSTCROFnlKrYw';
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-74.5, 40],
-        zoom: 8,
+        center: [ 103.90462168031246, 20.668780521951536 ],
+        zoom: 5,
     });
     const marker1 = new mapboxgl.Marker()
         .setLngLat([-74.5, 40])
@@ -131,18 +78,20 @@ function marker(){
 
 
 
-
+// Table
 document.getElementById("btn-seach").onclick=function ()
 {
     var timeStart =$("#date_timepicker_start").val();
-    var timeEnd = $("#date_timepicker_end").val();
+    var timeEnd =  $("#date_timepicker_end").val();
     var roles = $("#imeiDevice").val();
 
     var item = new FormData();
-    item.id=roles;
+    item.id = roles;
+    item.imei = $("#imeiDevice option:selected").text();
     item.start_time = timeStart;
     item.end_time = timeEnd;
-    item.imei = $("#imeiDevice option:selected").text();
+
+    console.log(1);
     console.log(item);
     var table = '<table class="table table-striped table-hover table-condensed table-export" id="table-history-view">\n' +
         '    <thead id="theadExport">\n' +
@@ -172,5 +121,3 @@ document.getElementById("btn-seach").onclick=function ()
         }
     })
 }
-
- */
